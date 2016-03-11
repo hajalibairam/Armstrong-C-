195266e9c14722cb73f51c1e537964327c50337b
Hajali Bairam <hajalibayram@outlook.com>
2016-03-11 19:15:02 +0400

    Update Armstrong.cpp

--- Armstrong.cpp
+++ Armstrong.cpp
@@ -2,22 +2,22 @@
 #include<conio.h>
 #include<cmath>
 
-int main(){
-int n, n1, temp=0, temp1=0,arm=0;
+  int main(){
+  int n, n1, temp=0, temp1=0,arm=0;
 
-scanf("%i",&n);
+  scanf("%i",&n);
 
-for(n1=n;n1!=0;n1/=10){
-temp++;
+  for(n1=n;n1!=0;n1/=10){
+    temp++;
 }
-for(n1=n;n1!=0;n1/=10){
-temp1=n1%10;
-arm+=pow(temp1,temp);
+  for(n1=n;n1!=0;n1/=10){
+   temp1=n1%10;
+    arm+=pow(temp1,temp);
 }
-if(arm==n)
-printf("True");
-else
-printf("False");
+  if(arm==n)
+    printf("True");
+  else
+    printf("False");
 
 }
 
